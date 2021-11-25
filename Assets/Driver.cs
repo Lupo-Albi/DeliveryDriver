6c59db2ba9c8247bb496f284923dcb13e70c5edb
Lupo-Albi <luandersonalbuquerque@gmail.com>
2021-11-24 21:27:36 -0300

    Finishing game

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
-  [SerializeField] float steerSpeed = 5f;
-  [SerializeField] float moveSpeed = 1f;
+  [SerializeField] float steerSpeed = 200f;
+  [SerializeField] float moveSpeed = 10f;
+  [SerializeField] float slowSpeed = 10f;
+  [SerializeField] float boostSpeed = 20f;
 
   // Start is called before the first frame update
   void Start()
@@ -20,6 +22,18 @@ public class Driver : MonoBehaviour
     float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
     transform.Rotate(0, 0, -steerAmount);
     transform.Translate(0, moveAmount, 0);
+  }
+
+  private void OnTriggerEnter2D(Collider2D other) {
+    if (other.tag == "Boost") {
+      moveSpeed = boostSpeed;
+    }
+  }
+
+  private void OnCollisionEnter2D(Collision2D other) {
+    if (other.gameObject.tag == "Bump") {
+      moveSpeed = slowSpeed;
+    }
   }
 }
 
